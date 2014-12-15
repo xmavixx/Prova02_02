faf605aebfdb1ed1192db6d96918f89aeb417a61
Wilson Horstmeyer Bogado <wilson@utfpr.edu.br>
2014-12-15 21:45:32 -0200

    Inicial

---
+++ src/main/java/Avaliacao2.java
@@ -0,0 +1,15 @@
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * IF62C - Fundamentos de Programação 2
+ * 
+ * Segunda avaliação parcial 2014/2.
+ * @author 
+ */
+public class Avaliacao2 {
+
+    public static void main(String[] args) {
+
+    }
+    
+}
---
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/Ponto.java
@@ -0,0 +1,55 @@
+package utfpr.ct.dainf.if62c.avaliacao;
+
+/**
+ * UTFPR - Universidade Tecnológica Federal do Paraná
+ * DAINF - Departamento Acadêmico de Informática
+ * IF62C - Fundamentos de Programação 2
+ * 
+ * Segunda avaliação parcial 2014/2.
+ * @author 
+ */
+public class Ponto {
+    private double x, y, z;
+
+    /**
+     * Retorna no nome não-qualificado da classe.
+     * @return O nome não qualificado da classe.
+     */
+    public String getNome() {
+        return getClass().getSimpleName();
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.x) ^ (Double.doubleToLongBits(this.x) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.y) ^ (Double.doubleToLongBits(this.y) >>> 32));
+        hash = 79 * hash + (int) (Double.doubleToLongBits(this.z) ^ (Double.doubleToLongBits(this.z) >>> 32));
+        return hash;
+    }
+
+}
